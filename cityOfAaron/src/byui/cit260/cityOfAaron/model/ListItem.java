98efa8f2167268b0cc3505c071f0b59ded9d0fa8
JIMENEZ'S <JIMENEZ'S@192.168.0.16>
2018-05-17 13:42:19 -0600

    5/17/2018 Nelson Jimenez added ListItem class

---
+++ cityOfAaron/src/byui/cit260/cityOfAaron/model/ListItem.java
@@ -0,0 +1,71 @@
+/*The main() class file for the cityOfAaron project 
+ *CIT-260 Brother Devry
+ *Team members: Johnny Medina, Nelson Jimenez, Jorge Trujillo
+ */
+
+package byui.cit260.cityOfAaron.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author JIMENEZ'S
+ */
+public class ListItem implements Serializable {
+    
+    private String name;
+    private int number;
+    
+    public ListItem() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + this.number;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "ListItem{" + "name=" + name + ", number=" + number + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ListItem other = (ListItem) obj;
+        if (this.number != other.number) {
+            return false;
+        }
+        return Objects.equals(this.name, other.name);
+    }
+    
+    
+    
+}
