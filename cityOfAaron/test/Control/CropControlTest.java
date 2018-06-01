304169217cb7b6af9cf92376d856e56e1092290d
medin <medin@192.168.1.10>
2018-05-31 18:41:37 -0600

    31-05-18 Johnny Medina, Jorge Trujillo, Nelson Jimenez: Added Control class and tested all buyLand test matrix required.

--- cityOfAaron/test/Control/CropControlTest.java
+++ cityOfAaron/test/Control/CropControlTest.java
@@ -20,18 +20,132 @@ public class CropControlTest {
     @Test
     public void testBuyLand() {
         System.out.println("buyLand");
+        
+        //Test case 1 (Valid, return expected result)
+        System.out.println("/tTest Case 1");
+        //Input variables
         CropData theCrops = new CropData();
         theCrops.setWheatInStore(1000);
         theCrops.setAcresOwned(2800);
         theCrops.setPopulation(500);
         int landPrice = 17;
         int acresToBuy = 50;
-        //CropData cropData = null;
+        
+        //Expected output variable
         int expResult = 2850;
+        
+        //Call the method
         int result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        
+
+        
+       //Test case 2 (Invalid, acers to buy is less than zero, return -1)
+        System.out.println("/tTest case 2");
+        
+        //Input variables
+        theCrops.setWheatInStore(1000);
+        theCrops.setAcresOwned(2800);
+        theCrops.setPopulation(500);
+        landPrice = 17;
+        acresToBuy = -50;
+        
+        //Expected output variable
+        expResult = -1;
+        
+        //Call the method
+        result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
+        assertEquals(expResult, result); 
+        
+        
+        
+        //Test case 3 (Invalid, not enough wheat in store to buy land, return -1)
+        System.out.println("/tTest case 3");
+        
+        //Input variables
+        theCrops.setWheatInStore(1000);
+        theCrops.setAcresOwned(2800);
+        theCrops.setPopulation(500);
+        landPrice = 17;
+        acresToBuy = 100;
+        
+        //Expected output variable
+        expResult = -1;
+        
+        //Call the method
+        result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
+        assertEquals(expResult, result);
+        
+        
+        
+        //Test case 4 (Invalid, not enough people to tend the land, return -1)
+        System.out.println("/tTest case 4");
+        
+        //Input variables
+        theCrops.setWheatInStore(1000);
+        theCrops.setAcresOwned(2800);
+        theCrops.setPopulation(500);
+        landPrice = 17;
+        acresToBuy = 100;
+        
+        //Expected output variable
+        expResult = -1;
+        
+        //Call the method
+        result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
+        assertEquals(expResult, result);
+        
+        
+        
+        //Test case 5 (Boundary, one less acer would return error, return expected result)
+        System.out.println("/tTest case 5");
+        
+        //Input variables
+        theCrops.setWheatInStore(1000);
+        theCrops.setAcresOwned(2800);
+        theCrops.setPopulation(500);
+        landPrice = 17;
+        acresToBuy = 0;
+        
+        //Expected output variable
+        expResult = 2800;
+        
+        //Call the method
+        result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
+        assertEquals(expResult, result);
+        
+        
+        
+        //Test case 6 (Boundary, one more bushel needed would return error, return expected result)
+        System.out.println("/tTest case 6");
+        
+        //Input variables
+        theCrops.setWheatInStore(1000);
+        theCrops.setAcresOwned(2800);
+        theCrops.setPopulation(500);
+        landPrice = 17;
+        acresToBuy = 58;
+        
+        //Expected output variable
+        expResult = 2858;
+        
+        //Call the method
+        result = CropControl.buyLand(landPrice, acresToBuy, theCrops);
+        
+        //test return to equal the expected result
+        assertEquals(expResult, result);
     }
     
+    
+        
 }
